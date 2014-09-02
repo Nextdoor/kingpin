e5be530d338e30bea65e57c61f4166cb3d16597e
Matt Wise <matt@nextdoor.com>
2014-09-01 20:29:14 -0700

    Convert aws.elb package to use tornado.concurrent.run_on_executor().

--- kingpin/actors/aws/elb.py
+++ kingpin/actors/aws/elb.py
@@ -18,7 +18,10 @@ import logging
 import math
 
 from boto.ec2 import elb as aws_elb
+from concurrent import futures
+from tornado import concurrent
 from tornado import gen
+from tornado import ioloop
 
 from kingpin import utils
 from kingpin.actors import base
@@ -30,19 +33,32 @@ log = logging.getLogger(__name__)
 __author__ = 'Mikhail Simin <mikhail@nextdoor.com>'
 
 
+# This executor is used by the tornado.concurrent.run_on_executor()
+# decorator. We would like this to be a class variable so its shared
+# across RightScale objects, but we see testing IO errors when we
+# do this.
+EXECUTOR = futures.ThreadPoolExecutor(10)
+
+
 # Helper function
 def p2f(string):
     """Convert percentage string into float.
 
     Converts string like '78.9%' into 0.789
     """
-    return float(string.strip('%'))/100
+    return float(string.strip('%')) / 100
 
 
 class WaitUntilHealthy(base.BaseActor):
+
     """Waits till a specified number of instances are "InService"."""
 
     required_options = ['name', 'count', 'region']
+
+    # Get references to existing objects that are used by the
+    # tornado.concurrent.run_on_executor() decorator.
+    ioloop = ioloop.IOLoop.current()
+    executor = EXECUTOR
 
     def __init__(self, *args, **kwargs):
         """Set up connection object.
@@ -81,16 +97,14 @@ class WaitUntilHealthy(base.BaseActor):
 
         return match[0]
 
-    @gen.coroutine
+    @concurrent.run_on_executor
     def _find_elb(self, name):
         """Return an ELB with the matching name.
 
         Must find exactly 1 match. Zones are limited by the AWS credentials"""
         self._log(logging.INFO, 'Searching for ELB "%s"' % name)
 
-        elbs = yield utils.thread_coroutine(
-            self.conn.get_all_load_balancers,
-            load_balancer_names=name)
+        elbs = self.conn.get_all_load_balancers(load_balancer_names=name)
         self._log(logging.INFO, 'ELBs found: %s' % elbs)
 
         if len(elbs) != 1:
@@ -98,7 +112,7 @@ class WaitUntilHealthy(base.BaseActor):
                 ('Expected to find exactly 1 ELB. Found %s: %s' %
                  (len(elbs), elbs)))
 
-        raise gen.Return(elbs[0])
+        return elbs[0]
 
     def _get_expected_count(self, count, total_count):
         """Calculate the expected count for a given percentage.
@@ -113,7 +127,7 @@ class WaitUntilHealthy(base.BaseActor):
 
         return expected_count
 
-    @gen.coroutine
+    @concurrent.run_on_executor
     def _is_healthy(self, elb, count):
         """Check if there are `count` InService instances for a given elb.
 
@@ -126,8 +140,7 @@ class WaitUntilHealthy(base.BaseActor):
                   ('Counting ELB InService instances for : %s' % name))
 
         # Get all instances for this ELB
-        instance_list = yield utils.thread_coroutine(
-            elb.get_instance_health)
+        instance_list = elb.get_instance_health()
         total_count = len(instance_list)
 
         self._log(logging.DEBUG, 'All instances: %s' % instance_list)
@@ -141,7 +154,7 @@ class WaitUntilHealthy(base.BaseActor):
         self._log(logging.INFO, 'InService vs expected: %s / %s' % (
                                 in_service_count, expected_count))
 
-        raise gen.Return(healthy)
+        return healthy
 
     @gen.coroutine
     def _execute(self):
