c76a377a7bd8bbb3fbd7ad0c24161e394ae84729
Matt Wise <matt@nextdoor.com>
2014-09-14 12:25:05 -0700

    Rename SQSRETRYDELAY to SQS_RETRY_DELAY

--- kingpin/actors/aws/settings.py
+++ kingpin/actors/aws/settings.py
@@ -27,4 +27,4 @@ __author__ = 'Mikhail Simin <mikhail@nextdoor.com>'
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID', '')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY', '')
 
-SQSRETRYDELAY = 30
+SQS_RETRY_DELAY = 30
