7d6786bfc50d12b5ba8f694551f6c4573d4d52da
Matt Wise <matt@nextdoor.com>
2015-02-12 07:34:36 -0800

    Bugfix: Fix parsing of unicode os.environ[] tokens at script parse time.
    I was expecting only strings, but did not account for unicode. Fixed up
    the code to support strings, unicode and bools. Added tests. Also added
    %USER% to the sleep.json script which is used in an integration test.
    %Without %USER% being parsed properly, that test will fail.

--- kingpin/test/test_utils.py
+++ kingpin/test/test_utils.py
@@ -27,6 +27,27 @@ class TestUtils(unittest.TestCase):
         result = utils.populate_with_tokens(string, tokens)
         self.assertEquals(result, expect)
 
+    def test_populate_with_unicode_env(self):
+        tokens = {'UNIT_TEST': u'FOOBAR'}
+        string = 'Unit %UNIT_TEST% Test'
+        expect = 'Unit FOOBAR Test'
+        result = utils.populate_with_tokens(string, tokens)
+        self.assertEquals(result, expect)
+
+    def test_populate_with_bool(self):
+        tokens = {'UNIT_TEST': True}
+        string = 'Unit %UNIT_TEST% Test'
+        expect = 'Unit True Test'
+        result = utils.populate_with_tokens(string, tokens)
+        self.assertEquals(result, expect)
+
+    def test_populate_with_bogus_data_OK(self):
+        tokens = {'UNIT_TEST': {'foobar': 'bat'}}
+        string = 'Unit %UNIT_TEST% Test'
+        expect = 'Unit %UNIT_TEST% Test'
+        result = utils.populate_with_tokens(string, tokens, strict=False)
+        self.assertEquals(result, expect)
+
     def test_populate_with_env_with_missing_variables(self):
         os.environ['UNIT_TEST'] = 'FOOBAR'
         string = 'Unit %UNIT_TEST% Test %NOTFOUNDVARIABLE%'
