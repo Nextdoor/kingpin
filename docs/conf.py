eac75577664db00c95cacd9596fde8acc4b4ec7f
Matt Wise <matt@nextdoor.com>
2015-04-30 16:53:54 -0700

    Remove sphinx.ext.napoleon: Not supported by RTD

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,6 @@ from kingpin import version as kingpin_version
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
@@ -38,18 +37,6 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
 ]
-
-# Napoleon settings
-napoleon_google_docstring = True
-napoleon_numpy_docstring = False
-napoleon_include_private_with_doc = False
-napoleon_include_special_with_doc = True
-napoleon_use_admonition_for_examples = False
-napoleon_use_admonition_for_notes = False
-napoleon_use_admonition_for_references = False
-napoleon_use_ivar = False
-napoleon_use_param = True
-napoleon_use_rtype = True
 
 # Get the class doc string from the class itself, not from the __init__ method
 autoclass_content = 'class'
