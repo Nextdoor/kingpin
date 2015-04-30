46a69d1f9973c6dc43e5a7b733cd55aada91161b
Matt Wise <matt@nextdoor.com>
2015-04-30 16:57:11 -0700

    Force the RTD theme at all times

--- docs/conf.py
+++ docs/conf.py
@@ -282,12 +282,7 @@ texinfo_documents = [
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
 
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-
-# On RTD we can't import sphinx_rtd_theme, but it will be applied by
-# default anyway.  This block will use the same theme when building locally
-# as on RTD.
-if not on_rtd:
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# Force the RTD theme for all builds
+import sphinx_rtd_theme
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
