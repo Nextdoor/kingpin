50953f52c04c3038674b35426e61a4803ebec509
Mikhail Simin <mikhail@nextdoor.com>
2015-06-23 18:20:10 -0700

    Nextdoorify Sphinx Documentation

--- docs/conf.py
+++ docs/conf.py
@@ -112,12 +112,43 @@ modindex_common_prefix = ['kingpin.', 'kingpin.actors.']
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'classic'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
-# documentation.
-#html_theme_options = {}
+# documentation: http://sphinx.readthedocs.org/en/latest/theming.html
+html_theme_options = {
+    'rightsidebar': False,
+    'stickysidebar': False,
+    'collapsiblesidebar': False,
+    'externalrefs': True,
+# There are also various color and font options that can change the color
+# scheme without having to write a custom stylesheet:
+    'relbarbgcolor': '#19975D',
+    'relbartextcolor': 'green',
+    'relbarlinkcolor': 'white',
+
+    'headbgcolor': '#f2f2f2',
+    'headtextcolor': 'black',
+    'headlinkcolor': '#c8c8c8',
+    #'headfont': '',
+
+    'sidebarbgcolor': '#f2f2f2',
+    'sidebarbtncolor': '#19975D',
+    'sidebartextcolor': '#818080',
+    'sidebarlinkcolor': 'black',
+
+    #'bgcolor': 'white',
+    #'textcolor': 'black',
+    #'bodyfont': '',
+    'codebgcolor': '#f8f8f8',
+    'codetextcolor': 'black',
+    'linkcolor': '#0061A5',
+    'visitedlinkcolor': '#0061A5',
+
+    'footerbgcolor': '#19975D',
+    'footertextcolor': 'white',
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
@@ -290,8 +321,3 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
-
-# Force the RTD theme for all builds
-import sphinx_rtd_theme
-html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
