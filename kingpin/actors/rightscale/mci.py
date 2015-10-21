0852105614266d97449b0d2830ce5dc0e435ba5b
Matt Wise <matt@nextdoor.com>
2015-10-21 14:01:10 -0700

    Actor for creating RightScale MCIs from simple JSON.

---
+++ kingpin/actors/rightscale/mci.py
@@ -0,0 +1,256 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Copyright 2014 Nextdoor.com, Inc
+
+"""
+:mod:`kingpin.actors.rightscale.mci`
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+.. _MultiCloudImages_:
+    http://reference.rightscale.com/api1.5/resources/ResourceMultiCloudImages.html
+    http://reference.rightscale.com/api1.5/resources/ResourceMultiCloudImageSettings.html
+"""
+
+import logging
+
+from tornado import gen
+
+from kingpin.actors import exceptions
+from kingpin.actors.rightscale import base
+from kingpin.constants import REQUIRED
+
+log = logging.getLogger(__name__)
+
+__author__ = 'Matt Wise <matt@nextdoor.com>'
+
+
+class MCIBaseActor(base.RightScaleBaseActor):
+
+    """Abstract MCI Actor that provides some utility methods."""
+
+
+class Create(MCIBaseActor):
+
+    """Creates a RightScale Multi Cloud Image.
+
+    Options match the documentation in RightScale:
+    http://reference.rightscale.com/api1.5/resources/ResourceDeployments.html
+
+    **Options**
+
+    :name:
+      The name of the MCI to be created.
+
+    :description:
+       The description of the MCI to be created.
+       (*optional*)
+
+    :images:
+       A list of dicts that each describe a single cloud and the image in that
+       cloud to launch. See below for details.
+
+    **Image Definitions**
+
+    Each cloud image definition is a dictionary that takes a few keys.
+
+    :cloud:
+      The name of the cloud as found in RightScale. We use the cloud 'Name'
+      which can be found in your `Settings -> Account Settings -> Clouds ->
+      insert_cloud_here` page. For example `AWS us-west-2`.
+
+    :image:
+      The cloud-specific Image UID. For example `ami-a1234abc`.
+
+    :instance_type:
+      The default instance type to launch when this AMI is launched. For
+      example, `m1.small`.
+      (*optional*)
+
+    :user_data:
+      The custom user data to pass to the instance on-bootup.
+      (*optional*)
+    """
+
+    all_options = {
+        'name': (str, REQUIRED, 'The name of the MCI to be created.'),
+        'description': (
+            str, '',
+            'The description of the MCI to be created.'),
+        'images': (
+            list, [],
+            'A list of objects that describe our per cloud image settings.'),
+    }
+
+    def __init__(self, *args, **kwargs):
+        """Validate the user-supplied parameters at instantiation time."""
+
+        super(Create, self).__init__(*args, **kwargs)
+
+        allowed_image_options = (
+            'cloud', 'image', 'instance_type', 'user_data')
+        required_image_options = ('cloud', 'image')
+
+        for image in self.option('images'):
+            # Sanity check that no extra options were passed in
+            for key in image.keys():
+                if key not in allowed_image_options:
+                    raise exceptions.InvalidOptions(
+                        'Invalid option (%s) found in Image %s' % (key, image))
+
+            # Make sure that the required options were passed in
+            for required in required_image_options:
+                if required not in image.keys():
+                    raise exceptions.InvalidOptions(
+                        'Missing option "%s" in Image %s' %
+                        (required, image))
+
+    @gen.coroutine
+    def _get_image_def(self, description):
+        """Returns a fully populated set of Multi Cloud Image settings.
+
+        This method takes in a dictionary with as set of parameters (cloud,
+        image, instance_type, user_data) and returns a fully ready-to-use
+        set of RightScale-formatted parameters to create that image. The method
+        handles discovering the RightScale HREFs for the cloud, image and
+        instance_type options.
+
+        Args:
+            description: A dictionary with the keys: cloud, image,
+            instance_type, user_data
+
+        Returns:
+            A RightScale-formatted array of tuples.
+        """
+
+        # Get our cloud object first -- its required so that we can search for
+        # the image/ramdisk/etc hrefs.
+        cloud = yield self._client.find_by_name_and_keys(
+            collection=self._client._client.clouds,
+            name=description['cloud'])
+        if not cloud:
+            raise exceptions.InvalidOptions(
+                'Invalid Cloud name supplied: %s' % description['cloud'])
+
+        # Find our image by searching for the resource_uid that matches.
+        image = yield self._client.find_by_name_and_keys(
+            collection=cloud.images,
+            resource_uid=description['image'])
+        if not image:
+            raise exceptions.InvalidOptions(
+                'Invalid cloud image name supplied: %s' % description['image'])
+
+        # Find our instance type now too
+        instance = yield self._client.find_by_name_and_keys(
+            collection=cloud.instance_types,
+            name=description['instance_type'])
+        if not instance:
+            raise exceptions.InvalidOptions(
+                'Invalid cloud instance_type supplied: %s' %
+                description['instance_type'])
+
+        # Generate our mci parameters, and each of the image settings
+        # parameters. This validates that our inputs are all correct one last
+        # time.
+        definition = self._generate_rightscale_params(
+            prefix='multi_cloud_image_setting',
+            params={
+                'cloud_href': cloud.href,
+                'image_href': image.href,
+                'instance_type_href': instance.href,
+                'user_data': description['user_data'],
+            })
+
+        self.log.debug('Prepared MCI Image Definition: %s' % definition)
+        raise gen.Return(definition)
+
+    @gen.coroutine
+    def _execute(self):
+
+        # Make sure the MCI doesn't already exist. If it does, we bail.
+        mci = yield self._client.find_by_name_and_keys(
+            collection=self._client._client.multi_cloud_images,
+            name=self.option('name'))
+        if mci:
+            raise exceptions.InvalidOptions(
+                'MCI "%s" already exists.' % self.option('name'))
+
+        # Generate the parameters for creating the top level MCI object
+        mci_params = self._generate_rightscale_params(
+            prefix='multi_cloud_image',
+            params={
+                'description': self.option('description'),
+                'name': self.option('name')
+            })
+
+        # Now, we need to validate that all of the inputs are correct by
+        # discovering the hrefs for each of the images supplied.
+        image_futures = []
+        for image in self.option('images'):
+            image_futures.append(self._get_image_def(image))
+        mci_settings_params = yield image_futures
+
+        # Finally, if we're dry, bail out..
+        if self._dry:
+            self.log.info('Would have created MCI: %s' % self.option('name'))
+            for setting in mci_settings_params:
+                self.log.info('Image Def: %s' % setting)
+            raise gen.Return()
+
+        # Ok, lets create this thing!
+        self.log.info('Creating MCI %s' % self.option('name'))
+        mci = yield self._client.create_resource(
+            self._client._client.multi_cloud_images, mci_params)
+
+        # Now add each of the image descriptions to the mci
+        for setting in mci_settings_params:
+            self.log.info('Creating MCI Setting: %s' % setting)
+            yield self._client.create_resource(
+                mci.settings, setting)
+
+
+class Destroy(MCIBaseActor):
+
+    """Deletes a RightScale MCI.
+
+    Options match the documentation in RightScale:
+    http://reference.rightscale.com/api1.5/resources/ResourceMultiCloudImages.html
+
+    **Options**
+
+    :name:
+      The name of the multi cloud image to be deleted.
+    """
+
+    all_options = {
+        'name': (str, REQUIRED,
+                 'The name of the multi cloud image to be deleted.'),
+    }
+
+    @gen.coroutine
+    def _execute(self):
+
+        mci = yield self._client.find_by_name_and_keys(
+            collection=self._client._client.multi_cloud_images,
+            name=self.option('name'))
+        if not mci:
+            raise exceptions.InvalidOptions(
+                'MCI "%s" does not exist.' % self.option('name'))
+
+        info = (yield self._client.show(mci.self)).soul
+
+        if self._dry:
+            self.log.info('Would delete MCI %s' % info['name'])
+            raise gen.Return()
+
+        self.log.info('Deleting MCI %s' % info['name'])
+        yield self._client.destroy_resource(mci)
---
+++ kingpin/actors/rightscale/test/test_mci.py
@@ -0,0 +1,202 @@
+import logging
+import mock
+
+from tornado import testing
+
+from kingpin.actors import exceptions
+from kingpin.actors.rightscale import base
+from kingpin.actors.rightscale import mci
+from kingpin.actors.test import helper
+
+log = logging.getLogger(__name__)
+
+
+class TestCreateActor(testing.AsyncTestCase):
+
+    def setUp(self, *args, **kwargs):
+        super(TestCreateActor, self).setUp()
+        base.TOKEN = 'unittest'
+
+        # Create some fake image reference objects
+        self._images = [
+            {'cloud': 'cloudA', 'image': 'ami-A',
+             'instance_type': 'm1.small', 'user_data': 'userdataA'},
+            {'cloud': 'cloudB', 'image': 'ami-B',
+             'instance_type': 'm1.small', 'user_data': 'userdataB'}
+        ]
+
+        # Create the actor
+        self.actor = mci.Create('Test',
+                                {'name': 'unit-test',
+                                 'description': 'unit test',
+                                 'images': self._images})
+
+        # Patch the actor so that we use the client mock
+        self.client_mock = mock.MagicMock()
+        self.actor._client = self.client_mock
+
+    @testing.gen_test
+    def test_bad_image_options(self):
+        bad = [
+            {'bad_option': 'cloudA', 'image': 'ami-A',
+             'instance_type': 'm1.small', 'user_data': 'userdataA'}]
+        missing = [
+            {'image': 'ami-A', 'user_data': 'userdataA'}]
+
+        with self.assertRaises(exceptions.InvalidOptions):
+            mci.Create('Test', {'name': 'test', 'images': bad})
+
+        with self.assertRaises(exceptions.InvalidOptions):
+            mci.Create('Test', {'name': 'test', 'images': missing})
+
+    @testing.gen_test
+    def test_get_image_def(self):
+        description = self._images[0]
+
+        # Create a few mocked objects that will be returned to help the method
+        # complete as if it made an API call.
+        cloud_mock = mock.MagicMock(name='cloudA')
+        cloud_mock.href = '/cloud_href'
+        image_mock = mock.MagicMock(name='ami-A')
+        image_mock.href = '/image_href'
+        instance_mock = mock.MagicMock(name='m1.small')
+        instance_mock.href = '/instance_href'
+
+        # What we expect this to look like when its all done
+        expected_result = [
+            ('multi_cloud_image_setting[cloud_href]', '/cloud_href'),
+            ('multi_cloud_image_setting[image_href]', '/image_href'),
+            ('multi_cloud_image_setting[instance_type_href]',
+                '/instance_href'),
+            ('multi_cloud_image_setting[user_data]', 'userdataA'),
+        ]
+
+        # Fake the find_by_name_and_keys method out so that the first time its
+        # called, it returns a mocked cloud object. The second time, it reutnrs
+        # a mocked image object. The third time it returns a mocked instance
+        # object.
+        actor = self.actor
+        with mock.patch.object(actor._client, 'find_by_name_and_keys') as cr:
+            cr.side_effect = [
+                helper.tornado_value(cloud_mock),
+                helper.tornado_value(image_mock),
+                helper.tornado_value(instance_mock)
+            ]
+
+            ret = yield actor._get_image_def(description)
+
+            self.assertEquals(ret, expected_result)
+
+    @testing.gen_test
+    def test_get_image_def_unable_to_find_hrefs(self):
+        description = self._images[0]
+
+        # Create a few mocked objects that will be returned to help the method
+        # complete as if it made an API call.
+        cloud_mock = mock.MagicMock(name='cloudA')
+        cloud_mock.href = '/cloud_href'
+        image_mock = mock.MagicMock(name='ami-A')
+        image_mock.href = '/image_href'
+        instance_mock = mock.MagicMock(name='m1.small')
+        instance_mock.href = '/instance_href'
+
+        # First test, nothing is returned on the cloud search
+        actor = self.actor
+        with mock.patch.object(actor._client, 'find_by_name_and_keys') as cr:
+            cr.return_value = helper.tornado_value(None)
+            with self.assertRaises(exceptions.InvalidOptions):
+                yield actor._get_image_def(description)
+
+        # Second test, a cloud is returned but no image is returned
+        with mock.patch.object(actor._client, 'find_by_name_and_keys') as cr:
+            cr.side_effect = [
+                helper.tornado_value(mock.MagicMock(name='cloud')),
+                helper.tornado_value(None),
+                helper.tornado_value(None),
+            ]
+            with self.assertRaises(exceptions.InvalidOptions):
+                yield actor._get_image_def(description)
+
+        # Final test, a cloud and image are returned but no instance type
+        with mock.patch.object(actor._client, 'find_by_name_and_keys') as cr:
+            cr.side_effect = [
+                helper.tornado_value(mock.MagicMock(name='cloud')),
+                helper.tornado_value(mock.MagicMock(name='image')),
+                helper.tornado_value(None),
+            ]
+            with self.assertRaises(exceptions.InvalidOptions):
+                yield actor._get_image_def(description)
+
+    @testing.gen_test
+    def test_exec_mci_exists(self):
+        self.actor._client.find_by_name_and_keys = helper.mock_tornado(1)
+        with self.assertRaises(exceptions.InvalidOptions):
+            yield self.actor._execute()
+
+    @testing.gen_test
+    def test_exec_dry(self):
+        self.actor._dry = True
+        self.actor._client.find_by_name_and_keys = helper.mock_tornado(None)
+        self.actor._get_image_def = helper.mock_tornado('fake_mci_params')
+
+        with mock.patch.object(self.actor._client, 'create_resource') as cr:
+            cr.return_value = 1
+            yield self.actor._execute()
+            self.assertEquals(
+                self.actor._client.create_resource._mock_call_count, 0)
+
+    @testing.gen_test
+    def test_exec(self):
+        self.actor._find_deployment = helper.mock_tornado(None)
+        self.actor._client.find_by_name_and_keys = helper.mock_tornado(None)
+        self.actor._get_image_def = helper.mock_tornado('fake_mci_params')
+        mci_mock = mock.MagicMock(name='fake-created-mci')
+
+        with mock.patch.object(self.actor._client, 'create_resource') as cr:
+            cr.side_effect = helper.mock_tornado(mci_mock)
+            yield self.actor._execute()
+            self.assertEquals(cr._mock_call_count, 3)
+
+
+class TestMCIDestroyActor(testing.AsyncTestCase):
+
+    def setUp(self, *args, **kwargs):
+        super(TestMCIDestroyActor, self).setUp()
+        base.TOKEN = 'unittest'
+
+        # Create the actor
+        self.actor = mci.Destroy('Test', {'name': 'unit-test'})
+
+        # Patch the actor so that we use the client mock
+        self.actor._client = mock.Mock()
+
+    @testing.gen_test
+    def test_exec_missing(self):
+        self.actor._client.find_by_name_and_keys = helper.mock_tornado(None)
+        with self.assertRaises(exceptions.InvalidOptions):
+            yield self.actor._execute()
+
+    @testing.gen_test
+    def test_exec_dry(self):
+        mci = mock.MagicMock(name='unit-test-mci')
+        self.actor._client.find_by_name_and_keys = helper.mock_tornado(mci)
+        self.actor._client.show = helper.mock_tornado(mock.MagicMock())
+        self.actor._dry = True
+
+        with mock.patch.object(self.actor._client, 'destroy_resource') as dr:
+            dr.return_value = 1
+            yield self.actor._execute()
+            self.assertEquals(
+                self.actor._client.destroy_resource._mock_call_count, 0)
+
+    @testing.gen_test
+    def test_exec(self):
+        mci = mock.MagicMock(name='unit-test-mci')
+        self.actor._client.find_by_name_and_keys = helper.mock_tornado(mci)
+        self.actor._client.show = helper.mock_tornado(mock.MagicMock())
+
+        with mock.patch.object(self.actor._client, 'destroy_resource') as dr:
+            dr.return_value = helper.tornado_value(1)
+            yield self.actor._execute()
+            self.assertEquals(
+                self.actor._client.destroy_resource._mock_call_count, 1)
