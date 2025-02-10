b14d9748ef9ba9c4ac4e10fd5a9ebbdc7047e4ed
Matt Wise <768067+diranged@users.noreply.github.com>
2025-02-10 08:11:58 -0800

    chore: update formatting for linting

--- kingpin/version.py
+++ kingpin/version.py
@@ -1,4 +1,3 @@
-"""Kingpin version number. You must bump this when creating a new release.
-"""
+"""Kingpin version number. You must bump this when creating a new release."""
 
 __version__ = "3.3.0"
